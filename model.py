58b97889d5e255d0082bd39c833d0b3a37af5ffd
Alexis <jeralexis77@aol.com>
2024-06-24 10:30:23 +0200

    model init + forward

--- model.py
+++ model.py
@@ -1,6 +1,35 @@
+import torch
+import torch.nn as nn
+
 class ConvNet(nn.Module):
     def __init__(self, input_size, n_kernels, output_size):
         super().__init__()
-        self.net = nn.Sequential()
+        
+        # Définition de l'architecture du réseau
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=n_kernels, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(in_channels=n_kernels, out_channels=n_kernels, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Flatten(),
+
+            nn.Linear(in_features=n_kernels * 4 * 4, out_features=50),
+            nn.ReLU(),
+
+            nn.Linear(in_features=50, out_features=output_size)
+        )
+
     def forward(self, x):
         return self.net(x)
+
+n_kernels = 6
+input_size = 28 * 28
+output_size = 10 
+
+model = ConvNet(input_size=input_size, n_kernels=n_kernels, output_size=output_size)
+
+print(model)
